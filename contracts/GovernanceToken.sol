1e08eb9040302c1ccb5cf6518da69a3e79c61de9
KhairallahA <khairallahfreelancer@gmail.com>
2022-08-05 19:29:25 +0300

    Deploy Contracts

---
+++ contracts/GovernanceToken.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+contract GovernanceToken is ERC20Votes {
+    uint256 public s_maxSupply = 1000000000000000000000000;
+
+    constructor() ERC20("GovernanceToken", "GT") ERC20Permit("GovernanceToken") {
+        _mint(msg.sender, s_maxSupply);
+    }
+
+    // The functions below are overrides required by Solidity.
+
+    /**
+     * @dev Move voting power when tokens are transferred.
+     */
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20Votes) {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    /**
+     * @dev Snapshots the totalSupply after it has been increased.
+     */
+    function _mint(address to, uint256 amount) internal override(ERC20Votes) {
+        super._mint(to, amount);
+    }
+
+    /**
+     * @dev Snapshots the totalSupply after it has been decreased.
+     */
+    function _burn(address account, uint256 amount) internal override(ERC20Votes) {
+        super._burn(account, amount);
+    }
+}
